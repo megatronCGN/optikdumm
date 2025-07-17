f3ec40dec745fa8d49f8c7759d6ca24afb03c866
Stefan Hilger <sh@webfactory.de>
2025-07-17 20:51:03 +0200

    added a lot of stuff

---
+++ .eleventy.js
@@ -0,0 +1,56 @@
+import path from "node:path";
+import * as sass from "sass";
+import eleventyNavigationPlugin from "@11ty/eleventy-navigation";
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+
+export default function (eleventyConfig) {
+    // Input directory: src
+    // Output directory: _site
+
+    // The following copies all files in `src`-folder to `_site`
+    eleventyConfig.addPassthroughCopy({"src/img/": 'img'});
+
+    eleventyConfig.addTemplateFormats("scss");
+
+    // Creates the extension for use
+    eleventyConfig.addExtension("scss", {
+        outputFileExtension: "css",
+
+        // opt-out of Eleventy Layouts
+        useLayouts: false,
+
+        compile: async function (inputContent, inputPath) {
+            let parsed = path.parse(inputPath);
+            // Don’t compile file names that start with an underscore
+            if(parsed.name.startsWith("_")) {
+                return;
+            }
+
+            let result = sass.compileString(inputContent, {
+                loadPaths: [
+                    parsed.dir || ".",
+                    this.config.dir.includes,
+                ]
+            });
+
+            // Map dependencies for incremental builds
+            this.addDependencies(inputPath, result.loadedUrls);
+
+            return async (data) => {
+                return result.css;
+            };
+        },
+    });
+
+    eleventyConfig.addPlugin(eleventyNavigationPlugin);
+
+    eleventyConfig.addPlugin(eleventyImageTransformPlugin);
+
+    return {
+        dir: {
+            input: 'src',
+            output: '_site'
+        }
+    }
+};
+
